26796fd8b0558dbc6f29109108c4b949556f00e4
Cthulu-in-a-tophat <kellnerj@student.psdr3.org>
2024-12-12 15:43:32 -0600

    hewwo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mauto.java
@@ -0,0 +1,124 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.SwitchableLight;
+
+
+
+@Autonomous(name="Auto", group="Robot")
+public class Mauto extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() {
+
+        /* Declare OpMode members. */
+        // Initialize the drive system variables.
+        DcMotor mMotor = hardwareMap.dcMotor.get("somethingBetter"); // arm
+        Servo mServo = hardwareMap.servo.get("badThing"); //claw
+
+        DcMotor motorFrontRight = hardwareMap.dcMotor.get("Front Right"); //
+        DcMotor motorBackRight = hardwareMap.dcMotor.get("Back Right");   //
+        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("Front Left"); //
+        DcMotor motorBackLeft = hardwareMap.dcMotor.get("Back Left");   //
+        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
+        // When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
+        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
+        motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        //drive forward code for one block:
+        /*
+        motorFrontRight.setPower(0.5);
+        motorBackRight.setPower(0.5);
+        motorFrontLeft.setPower(0.5);
+        motorBackLeft.setPower(0.5);
+        sleep(3000);
+        */
+
+        waitForStart();
+        //forward a bit
+        motorFrontRight.setPower(0.5);
+        motorBackRight.setPower(0.5);
+        motorFrontLeft.setPower(0.5);
+        motorBackLeft.setPower(0.5);
+        sleep(90);
+        //left one square
+        motorFrontRight.setPower(0.5);
+        motorBackRight.setPower(-0.5);
+        motorFrontLeft.setPower(-0.5);
+        motorBackLeft.setPower(0.5);
+        sleep(2000);
+        //forward two squares
+        motorFrontRight.setPower(0.5);
+        motorBackRight.setPower(0.5);
+        motorFrontLeft.setPower(0.5);
+        motorBackLeft.setPower(0.5);
+        sleep(2950);
+        //arm raise
+        mMotor.setPower(-0.2);
+        sleep(900);
+        //turn right
+        motorFrontRight.setPower(-0.5);
+        motorBackRight.setPower(-0.5);
+        motorFrontLeft.setPower(0.5);
+        motorBackLeft.setPower(0.5);
+        sleep(1500);
+        //forward
+        motorFrontRight.setPower(0.5);
+        motorBackRight.setPower(0.5);
+        motorFrontLeft.setPower(0.5);
+        motorBackLeft.setPower(0.5);
+        sleep(1050);
+        //lower arm
+        //arm raise
+        mMotor.setPower(0.2);
+        sleep(500);
+        //😂
+
+
+
+
+
+        //stop
+        motorFrontRight.setPower(0);
+        motorBackRight.setPower(0);
+        motorFrontLeft.setPower(0);
+        motorBackLeft.setPower(0);
+    }
+}
